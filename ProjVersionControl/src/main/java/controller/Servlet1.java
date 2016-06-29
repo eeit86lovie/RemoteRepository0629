869f33730a426cf67518ce378ab34e42455b0eed
張恬雲 <@III-ITC>
2016-06-29 15:53:35 +0800

    增加syso

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -16,7 +16,7 @@ public class Servlet1 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 
-
+  System.out.println("555");
 
 		HttpSession session = request.getSession();
 		session.setAttribute("time", ""+sDateFormat.format(new Date()));
