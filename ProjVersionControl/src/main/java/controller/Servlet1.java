5daa6b3ab811250c32f7a1bed881b7153171358f
彭開晨 <@EEIT86>
2016-06-29 15:51:35 +0800

    第N次修改

--- ProjVersionControl/src/main/java/controller/Servlet1.java
+++ ProjVersionControl/src/main/java/controller/Servlet1.java
@@ -17,7 +17,6 @@ public class Servlet1 extends HttpServlet {
 		
 
 
-		System.out.println("QQ");
 		System.out.println("第二次commit");
 		System.out.println("第三次commit");
 		System.out.println("最後一次commit");
